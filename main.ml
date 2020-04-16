4bef11c40da4d237f68d791643d3aeb7d8060496
Ian Palleiko <ian.palleiko@gmail.com>
2020-04-15 20:05:59 -0400

    Inline

--- main.ml
+++ main.ml
@@ -5,22 +5,19 @@ let makeparse parse =
     | 'd' -> Morph.Imperative
     | 'i' -> Morph.Imperfect
     | 'a' -> Morph.Aorist
-    | _ -> failwith "Bad input"
-    in
+    | _ -> failwith "Bad input" in
   let person =
     match parse.[1] with
     | '1' -> Morph.First
     | '2' -> Morph.Second
     | '3' -> Morph.Third
-    | _ -> failwith "Bad input"
-    in
+    | _ -> failwith "Bad input" in
   let number =
     match parse.[2] with
     | 's' -> Morph.Singular
     | 'd' -> Morph.Dual
     | 'p' -> Morph.Plural
-    | _ -> failwith "Bad input"
-    in
+    | _ -> failwith "Bad input" in
   (tense, person, number)
 
 let run request =
